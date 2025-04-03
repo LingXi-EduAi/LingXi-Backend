0c35de5d8f7bf37fb07027c98fa7e807d863b817
许浩峰 <1140155854@qq.com>
2025-04-03 16:58:33 +0800

    bug修改

--- src/main/java/com/lxe/lx/domain/qo/LXClassQO.java
+++ src/main/java/com/lxe/lx/domain/qo/LXClassQO.java
@@ -1,5 +1,10 @@
 package com.lxe.lx.domain.qo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LXClassQO extends BaseListQO{
     private String name;
     private String classGroupingId;
