dec20014b5a269843d2233ba838cee91f21f7912
许浩峰 <1140155854@qq.com>
2025-04-04 14:04:21 +0800

    bug修复

--- src/main/java/com/lxe/lx/service/ApiService.java
+++ src/main/java/com/lxe/lx/service/ApiService.java
@@ -1,5 +1,5 @@
 package com.lxe.lx.service;
 
-public interface ChatMessageService {
+public interface ApiService {
     public String sendMessage(String userInput, String userId, String conversationId)throws Exception;
 }
